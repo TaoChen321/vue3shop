a72007cdf6150759b30786cfd465a4584b66d0ea
TC <10898842+TccT123@user.noreply.gitee.com>
2022-10-27 09:42:05 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <!-- 原生 -->
+    <!-- 使用state的数据
+    <p>{{ $store.state.username }}</p>
+    使用getters的数据
+    <p>{{ $store.getters.newName }}</p>
+    <p>{{ $store.getters['newName'] }}</p>
+    调用mutations方法
+    <button @click="mutationsFn">调用mutations方法</button>
+    调用actions方法
+    <button @click="actionsFn">调用actions方法</button> -->
+    <!-- 模块 -->
+    模块A
+    <p>state：{{ $store.state.moduleA.username }}</p>
+    <p>getters：{{ $store.getters.newName }}</p>
+    <p>state需要模块名字,其他则不需要，除非开启了命名空间</p>
+    模块B
+    <p>{{ $store.state.moduleB.username }}</p>
+    <p>state：{{ $store.state.moduleB.username }}</p>
+    <p>下面开启了命名空间</p>
+    <p>getters：{{ $store.getters['moduleB/newName'] }}</p>
+    <button @click="mutationsFn">mutationsFn改变</button>
+    <button @click="actionsFn">actionsFn改变</button>
+  </div>
 </template>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script setup>
+import { useStore } from 'vuex'
+// 原生
+const store = useStore()
+// // 使用state的数据
+// console.log(store.state.username)
+// // 使用getters的数据
+// console.log(store.getters.newName)
+// // 调用mutations方法
+// const mutationsFn = () => {
+//   store.commit('updateName')
+// }
+// const actionsFn = () => {
+//   store.dispatch('updateName')
+// }
+// 模块
+const mutationsFn = () => {
+  store.commit('moduleB/updateName')
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+const actionsFn = () => {
+  store.dispatch('moduleB/updateName')
 }
-</style>
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,73 @@
 import { createStore } from 'vuex'
-
-export default createStore({
+// A模块
+const moduleA = {
   state: {
+    username: 'moduleA'
   },
   getters: {
+    newName (state) {
+      return state.username + 'AAA'
+    }
   },
   mutations: {
+    updateName (state) {
+      state.username = 'moduleAAAAAA'
+    }
+  }
+}
+// B模块
+const moduleB = {
+  namespaced: true,
+  state: {
+    username: 'moduleB'
+  },
+  getters: {
+    newName (state) {
+      return state.username + 'BBB'
+    }
+  },
+  mutations: {
+    updateName (state) {
+      state.username = 'moduleAAAAAA'
+    }
   },
   actions: {
-  },
+    updateName (ctx) {
+      setTimeout(() => {
+        ctx.commit('updateName')
+      }, 300)
+    }
+  }
+}
+
+export default createStore({
   modules: {
+    moduleA,
+    moduleB
   }
 })
+// export default createStore({
+//   state: {
+//     username: '张三'
+//   },
+//   getters: {
+//     newName (state) {
+//       return state.username + '李四'
+//     }
+//   },
+//   mutations: {
+//     updateName (state) {
+//       state.username = '李四'
+//     }
+//   },
+//   actions: {
+//     updateName (ctx) {
+//       // 发请求
+//       setTimeout(() => {
+//         ctx.commit('updateName')
+//       }, 500)
+//     }
+//   },
+//   modules: {
+//   }
+// })
